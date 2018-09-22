ad06fdd0ff5eb8309251de199290b3c844bc6339
qinlinsen <qinlinsen@cloudwalk.cn>
2018-09-22 17:29:45 +0800

    build spring mvc framework !

--- spring-core-indepth-web/src/main/java/hello/GreetingController.java
+++ spring-core-indepth-web/src/main/java/hello/GreetingController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Abraham Qin
+ */
 @RestController
 public class GreetingController {
 
